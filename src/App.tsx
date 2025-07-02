eb87059eae6324dad4a54bbd030264b3714350ef
bennettbijumathew <bennettbijumathew@gmail.com>
2025-07-02 14:33:49 +1000

    Added Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from "./assets/react.svg";
 // import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
-import TaskList from "./pages/TaskList";
+import TaskList from "./pages/task-list";
 
 function App() {
     return (
---
+++ src/pages/task-list.tsx
@@ -0,0 +1,21 @@
+function TaskList() {
+    return (
+        <main className="flex h-screen p-4 gap-x-4 bg-zinc-900">
+            <section className="flex flex-col flex-1 gap-y-4">
+                <div className="h-32 p-4 border border-zinc-700 rounded-lg">
+                    <p> DOCKET. </p>
+                </div>
+
+                <div className="flex-1 p-4 border border-zinc-700 rounded-lg">
+
+                </div>
+            </section>
+
+            <section className="flex-3 p-4 border border-zinc-700 rounded-lg">
+
+            </section>
+        </main>
+    )
+}
+
+export default TaskList;
