0bfbd46e57915a25d4804818fbefcd777a024837
bennettbijumathew <bennettbijumathew@gmail.com>
2026-07-08 22:35:22 +1000

    feat (settings): added notification tile to settings page for android fix (settings): made styling more cleaner

--- src/components/ui/settings/account-tile.svelte
+++ src/components/ui/settings/account-tile.svelte
@@ -5,12 +5,16 @@
 </script>
 
 <div class="
-    flex items-center justify-between gap-x-3 text-left bg-background-50 p-3 rounded-lg
-    min-h-24 px-2 gap-2
-    sm:min-h-22 sm:px-3 sm:gap-3
+    flex justify-between gap-x-3 text-left bg-background-50 p-3 rounded-lg
+    min-h-24 px-2 gap-2 flex-col
+    sm:min-h-22 sm:px-3 sm:gap-3 sm:flex-row sm:items-center
     lg:min-h-16
 ">
-    <div class="flex items-center">
+    <div class="
+        flex items-center
+        flex-1
+        sm:flex-2
+    ">
         <CircleUser class="size-7"/>
         
         <div class="pl-3">
@@ -25,19 +29,27 @@
         </div>
     </div>
     
-    <div class="flex items-center gap-x-2">
+    <div class="flex items-center gap-x-2 ">
         <a 
             href={p("/login")}
             aria-label="Change user accounts"
-            class="flex justify-between items-center gap-x-3 rounded-lg p-1.5 px-3 bg-background-200 hover:bg-background-300 hover:cursor-pointer"
+            class="
+                flex justify-between items-center gap-x-3 rounded-lg p-1.5 px-3 bg-background-200 hover:bg-background-300 hover:cursor-pointer
+                flex-1
+                sm:flex-none
+            "
         >
-            Change Accounts
+            Change User
         </a>
         
         <a 
             href={p("/logout")}
             aria-label="Log out of your current website"
-            class="flex justify-between items-center gap-x-3 rounded-lg p-1.5 px-3 bg-background-200 hover:bg-background-300 hover:cursor-pointer"
+            class="
+                flex justify-between items-center gap-x-3 rounded-lg p-1.5 px-3 bg-background-200 hover:bg-background-300 hover:cursor-pointer
+                flex-1
+                sm:flex-none
+            "
         >
             Log Out
         </a>
